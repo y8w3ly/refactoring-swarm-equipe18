093ea2c2694952281e431778e0d432d73c0ba1cf
femto <y8w3ly@urmomsfavpc.esi-sba.dz>
2026-01-28 16:44:22 +0100

    added results of the script buggycodes

--- buggycodes/3.py
+++
@@ -1,4 +0,0 @@
-def read_file(f):
-    file = open(f, 'r')
-    data = file.read()
-    print(data)
--- buggycodes/8.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def fetch_data(url):
-    r = requests.get(url)
-    return r.json()
