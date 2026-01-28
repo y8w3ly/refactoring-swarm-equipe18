cb35f1cdf0e18e2f0ae5096c48d4190192db2273
femto <y8w3ly@urmomsfavpc.esi-sba.dz>
2026-01-28 16:42:08 +0100

    added script that generates dataset

--- buggycodes/2.py
+++
@@ -1,6 +0,0 @@
-def is_palindrome(s):
-    rev = ""
-    for i in range(len(s)):
-        rev += s[len(s)-1-i]
-    if rev == s:
-        return True
--- buggycodes/9.py
+++
@@ -1,4 +0,0 @@
-def merge_dicts(d1, d2):
-    for k in d2:
-        d1[k] = d2[k]
-    return d1
