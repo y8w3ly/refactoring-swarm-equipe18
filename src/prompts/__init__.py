16ac423d473f2caed56cb438e5dab74523446e82
femto <y8w3ly@urmomsfavpc.esi-sba.dz>
2026-01-28 15:35:04 +0100

    needed stuff

---
+++ src/prompts/__init__.py
@@ -0,0 +1,9 @@
+from src.prompts.auditor_prompt import AUDITOR_SYSTEM_PROMPT
+from src.prompts.fixer_prompt import FIXER_SYSTEM_PROMPT
+from src.prompts.judge_prompt import JUDGE_SYSTEM_PROMPT
+
+__all__ = [
+    "AUDITOR_SYSTEM_PROMPT",
+    "FIXER_SYSTEM_PROMPT",
+    "JUDGE_SYSTEM_PROMPT"
+]
